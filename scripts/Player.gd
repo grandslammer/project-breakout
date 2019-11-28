abe9038966005accdbab99c6c64374ab04b4f9b1
Richard <richard.mcnulty10@gmail.com>
2019-11-29 00:37:41 +1300

    added mouse input to paddle movement
    the fast the paddle hits the ball the more momentum is transfered into the ball and therefore increased speed

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,30 +1,24 @@
-extends KinematicBody2D
+extends RigidBody2D
 
-var motion = Vector2()
-const PLAYERSPEED = 500
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	update_motion()
-	
 
+func _input(event):
+    if event is InputEventScreenDrag:
+        print("mouse button event at ", event.position)
 
 func update_motion():
-	if Input.is_action_pressed("ui_left") and not Input.is_action_pressed("ui_right"):
-		motion.x = -PLAYERSPEED
-	elif Input.is_action_pressed("ui_right") and not Input.is_action_pressed("ui_left"):
-		motion.x = PLAYERSPEED
-	else:
-		motion.x = 0
-	move_and_slide(motion)
+	self.position.x = get_global_mouse_position().x
 	
 func changeBallDirection(ballObject):
 	var anchor = get_node("Anchor")
 	var speed = ballObject.get_linear_velocity().length()
+	var paddleSpeed = self.get_linear_velocity().length()
 	var direction = ballObject.position - anchor.global_position
-	var velocity = direction.normalized()*500
+	var velocity = direction.normalized()*(500+(paddleSpeed/5))
 	ballObject.set_linear_velocity(velocity)
